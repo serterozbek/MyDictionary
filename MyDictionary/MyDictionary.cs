d6c6adc977fa14b171d633626121bc1d3ebbbfe7
serterozbek <serter.ozbek@hotmail.com>
2021-02-27 18:55:32 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] keyItems;
+        T2[] valueItems;
+        public MyDictionary()
+        {
+            keyItems = new T1[0];
+            valueItems = new T2[0];
+        }
+        public void Add (T1 itemkey, T2 itemvalue)
+        {
+            T1[] tempArrayKey = keyItems;
+            T2[] tempArrayValue = valueItems;
+
+            keyItems = new T1[keyItems.Length + 1];
+            valueItems = new T2[valueItems.Length + 1];
+
+            for(int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keyItems[i] = tempArrayKey[i];
+            }
+
+            for(int i = 0; i < tempArrayValue.Length; i++)
+            {
+                valueItems[i] = tempArrayValue[i];
+            }
+
+            keyItems[keyItems.Length - 1] = itemkey;
+            valueItems[valueItems.Length - 1] = itemvalue;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> musteriler = new MyDictionary<int, string>();
+            musteriler.Add(105, "Serter Özbek");
+            musteriler.Add(205, "Osman Gümüş");
+            musteriler.Add(15, "Yunus Demirel");
+            musteriler.Add(58, "Okan Dursun");
+ 
+        }
+    }
+}
